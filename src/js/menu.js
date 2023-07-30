5d1d4a53ac331f8f1e34e22900f3e68ca6a62315
foaxdev <foaxdev@yahoo.com>
2023-07-30 12:50:36 +0200

    Added better menu to the header

---
+++ src/js/menu.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const icon = document.querySelector('.menu-icon');
+  const menu = document.querySelector('.menu');
+  const mobileWidth = 767;
+
+  if (icon !== null && menu !== null) {
+    menu.classList.toggle('menu--closed', window.innerWidth < mobileWidth);
+    window.addEventListener('resize', windowResize);
+    icon.addEventListener('click', toggle);
+
+    function toggle() {
+      menu.classList.toggle('menu--closed');
+    }
+
+    function windowResize() {
+      menu.classList.toggle('menu--closed', window.innerWidth < mobileWidth);
+    }
+  }
+});
